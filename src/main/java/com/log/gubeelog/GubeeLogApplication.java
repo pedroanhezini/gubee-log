9e22b47af489891f0a6ff8e51ce1428e060d609e
pedroanhezini <pedroanhezini@hotmail.com>
2020-05-08 14:51:58 -0300

    Configuration json console log

--- src/main/java/com/log/gubeelog/GubeeLogApplication.java
+++ src/main/java/com/log/gubeelog/GubeeLogApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GubeeLogApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GubeeLogApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(GubeeLogApplication.class, args);
+    }
 }
---
+++ src/main/java/com/log/gubeelog/MyClass.java
@@ -0,0 +1,11 @@
+package com.log.gubeelog;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MyClass {
+    private String name;
+    private Integer age;
+}
