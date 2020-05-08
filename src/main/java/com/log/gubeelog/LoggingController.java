63b1cae85b4db9b319090d6d69c0dc25737880f8
pedroanhezini <pedroanhezini@hotmail.com>
2020-05-08 14:51:11 -0300

    Configuration json console log

--- src/main/java/com/log/gubeelog/LoggingController.java
+++ src/main/java/com/log/gubeelog/LoggingController.java
@@ -5,21 +5,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static net.logstash.logback.argument.StructuredArguments.keyValue;
+import static net.logstash.logback.argument.StructuredArguments.fields;
 
 
 @RestController
 public class LoggingController {
 
-    private Logger logger = LoggerFactory.getLogger("jsonLogger");
+    private Logger logger = LoggerFactory.getLogger("integrationLogger");
 
     @RequestMapping("log/")
     public String index() {
-        logger.trace("A TRACE Message {}", keyValue("orderId", "123"));
-        logger.debug("A DEBUG Message {}", keyValue("orderId", "123"));
-        logger.info("An INFO Message");
-        logger.warn("A WARN Message");
-        logger.error("An ERROR Message");
+        MyClass myClass = new MyClass("Pedro", 22);
+        logger.info("An INFO Message {}", fields(myClass));
         return "Howdy! Check out the Logs to see the output...";
     }
 }
